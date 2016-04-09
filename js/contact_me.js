fef47bfae22179988168cdfc58a6e2f5e9943161
Scott Swarthout <scott.swarthout724@gmail.com>
2016-04-08 20:02:50 -0400

    changed email provider

--- js/contact_me.js
+++ js/contact_me.js
@@ -1,49 +1,42 @@
-$(function() {
+$(function () {
     $("input,textarea").jqBootstrapValidation({
         preventSubmit: true,
-        submitError: function($form, event, errors) {
+        submitError: function ($form, event, errors) {
             // additional error messages or events
         },
-        submitSuccess: function($form, event) {
+        submitSuccess: function ($form, event) {
             event.preventDefault(); // prevent default submit behaviour
             // get values from FORM
             var name = $("input#name").val();
             var email = $("input#email").val();
             var phone = $("input#phone").val();
             var message = $("textarea#message").val();
-			var email_body = "You have received a new message from your website contact form.\n\n" + 
-				"Here are the details:\n\nName: " + name + 
-				"\n\nEmail: " + email +
-				"\n\nPhone: " + phone + 
-				"\n\nMessage:\n" + message
-			
+            var email_body = "You have received a new message from your website contact form.\n\n" +
+                "Here are the details:\n\nName: " + name +
+                "\n\nEmail: " + email +
+                "\n\nPhone: " + phone +
+                "\n\nMessage:\n" + message;
+
             var firstName = name; // For Success/Failure Message
             // Check for white space in name for Success/Fail message
-            if(firstName.indexOf(' ') >= 0) {
+            if (firstName.indexOf(' ') >= 0) {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "https://mandrillapp.com/api/1.0/messages/send.json",
-                type: "POST",
+                url: "https://formspree.io/you@email.com",
+                method: "POST",
+                data: {message: "hello!"},
+                dataType: "json"
+            });
+            $.ajax({
+                url: "https://formspree.io/scott.swarthout724@gmail.com",
+                method: "POST",
                 data: {
-                    'key': '56Y39zNqNOgZrNNXRkAA1w',
-                    'message': {
-                        'from_email': "halcyonlabsllc@gmail.com",
-                        'from_name': "Halcyon Labs Web Contact",
-                        'headers': {
-                            'Reply-To': email
-                        },
-                        'subject': "Website Contact Form: " + name,
-                        'text': email_body,
-                        'to': [{
-                            'email': 'scott.swarthout724@gmail.com',
-                            'name': 'Scott Swarthout',
-                            'type': 'to'
-                        }]
-                    }
+                    'message': email_body
                 },
+                dataType: "json",
                 cache: false,
-                success: function() {
+                success: function () {
                     // Success message
                     $('#success').html("<div class='alert alert-success'>");
                     $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;").append("</button>");
@@ -52,7 +45,7 @@ $(function() {
                     //clear all fields
                     $('#contactForm').trigger("reset");
                 },
-                error: function() {
+                error: function () {
                     // Fail message
                     $('#success').html("<div class='alert alert-danger'>");
                     $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;").append("</button>");
@@ -60,23 +53,23 @@ $(function() {
                     $('#success > .alert-danger').append('</div>');
                     //clear all fields
                     $('#contactForm').trigger("reset");
-                },
+                }
             })
         },
-        filter: function() {
+        filter: function () {
             return $(this).is(":visible");
-        },
+        }
     });
-    $("a[data-toggle=\"tab\"]").click(function(e) {
+    $("a[data-toggle=\"tab\"]").click(function (e) {
         e.preventDefault();
         $(this).tab("show");
     });
 });
 /*When clicking on Full hide fail/success boxes */
-$('#name').focus(function() {
+$('#name').focus(function () {
     $('#success').html('');
 });
 /*When clicking on Full hide fail/success boxes */
-$('#name').focus(function() {
+$('#name').focus(function () {
     $('#success').html('');
 });
